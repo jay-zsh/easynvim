e10b1f9c544e9085fdf6cbe988f015a828118d21
zsh <howardchou49@gmail.com>
2025-10-16 15:44:55 +0800

    release3.1

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,31 +3,37 @@ return {
     cmd = "Telescope",  -- cmd Telescope` 懒加载
     dependencies = {
         "https://github.com/nvim-lua/plenary.nvim",
-        {
-            "https://github.com/nvim-telescope/telescope-fzf-native.nvim",
-            build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && "
-                .. "cmake --build build --config Release && "
-                .. "cmake --install build --prefix build",
-        },
     },
     keys = {
-        { "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "Find Files (Telescope)" }, -- 按键时懒加载
-        { "<leader>fg", "<cmd>Telescope live_grep<CR>", desc = "Live Grep" },
+        { "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "Find Files (Telescope)" },
+        { "<leader>fg", function() 
+            -- 检查ripgrep是否可用
+            local has_rg = false
+            if vim.fn.executable('rg') == 1 or vim.fn.executable('ripgrep') == 1 then
+                has_rg = true
+            end
+            
+            if has_rg then
+                -- ripgrep可用，使用live_grep
+                vim.cmd('Telescope live_grep')
+            else
+                -- ripgrep不可用，使用普通的find_files替代
+                vim.notify("ripgrep未安装，使用文件查找替代。推荐安装ripgrep以获得更好的搜索体验。", vim.log.levels.WARN)
+                vim.cmd('Telescope find_files')
+            end
+        end, desc = "Live Grep (Safe)" },
     },
     opts = {
-        extensions = {
-            fzf = {
-                fuzzy = true,
-                override_generic_sorter = true,
-                override_file_sorter = true,
-                case_mode = "smart_case",
+        defaults = {
+            sorting_strategy = "ascending",
+            layout_config = {
+                prompt_position = "top",
             },
+            -- 移除不正确的sorter配置，使用默认值
         },
     },
     config = function(_, opts)
         local telescope = require "telescope"
         telescope.setup(opts)
-        telescope.load_extension("fzf")
-        -- require('telescope').load_extension('fzf')
     end,
 }
