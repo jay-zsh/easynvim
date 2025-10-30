6170e08cebf1f48e0559b2d64a022935cffb4267
jay-zsh <p204654297@outlook.com>
2025-10-30 09:55:09 +0800

    release3.14

--- init.lua
+++ init.lua
@@ -1,4 +1,10 @@
-require("core.basic")
-require("core.keymap")
-require("core.lazy")
+-- 设置包路径，确保能正确加载 lua 模块
+package.path = package.path .. ';' .. vim.fn.stdpath('config') .. '/lua/?.lua'
+
+-- 加载核心配置
+require('core.basic')
+require('core.keymap')
+
+-- 加载插件管理器
+require('core.lazy')
 
